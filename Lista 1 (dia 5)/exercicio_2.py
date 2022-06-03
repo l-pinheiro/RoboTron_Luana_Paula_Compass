bf933873b66a8c4109990f3562f2449a8920dd74
Luana Pinheiro <lpinheiro.correa@gmail.com>
2022-06-03 09:29:07 -0300

    adicionando exercicio 2 (L1)

---
+++ Lista 1 (dia 5)/exercicio_2.py
@@ -0,0 +1,9 @@
+""""2- Construa um programa que armazena em duas variaveis duas notas e apresenta a média entre as duas"""
+
+# armazena duas notas em variáveis diferentes
+nota_1 = float(input("Digite a 1ª nota: "))
+nota_2 = float(input("Digite a 2ª nota: "))
+
+media = (nota_1 + nota_2)/2 # calcula a média das notas
+
+print("\nA média das notas é: ", media) # apresenta o valor calculado da média
