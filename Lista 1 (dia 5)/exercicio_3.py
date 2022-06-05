e2c48568ebe6d13d05910f77bed5b5097ee17f94
Luana Pinheiro <lpinheiro.correa@gmail.com>
2022-06-05 08:00:02 -0300

    adicionando exercicio 3 (L1)

---
+++ Lista 1 (dia 5)/exercicio_3.py
@@ -0,0 +1,14 @@
+"""3- Construa um programa que recebe dois valores, soma esses valores e apresenta
+se o resultado é par ou impar"""
+
+# recebe dois valores inteiros
+valor_1 = int(input("Digite o 1ª valor: "))
+valor_2 = int(input("Digite o 2ª valor: "))
+
+soma = valor_1 + valor_2 # calcula a soma dos valores
+
+# condição para apresentar se o resultado é par ou ímpar
+if soma % 2 == 0:
+    print(f'O resultado da soma é {soma}, logo é par')
+else:
+    print(f'O resultado da soma é {soma}, logo é ímpar')
