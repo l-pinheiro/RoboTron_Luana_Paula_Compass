fe482f9437d6c5014d6cf84c84e142320a66804d
Luana Pinheiro <lpinheiro.correa@gmail.com>
2022-06-05 20:31:29 -0300

    adicionando exercicio 10 (L1)

---
+++ Lista 1 (dia 5)/exercicio_10.py
@@ -0,0 +1,18 @@
+"""10- Crie um programa que recebe uma lista com três frutas e compare com uma
+lista com os valores ["maça", "banana", "pera"]"""
+
+frutas = []
+frutas_fixas = ["maça", "banana", "pera"]
+
+# laço que recebe as frutas e as armazena em uma lista
+for n in range(3):
+    frutas.append((input(f'Digite a {n+1}ª fruta: ')))
+
+# transforma as listas em conjuntos e compara quais os elementos que pertencem a ambos
+frutas_iguais = set(frutas) & set(frutas_fixas)
+
+# condição que verifica se alguma fruta coincidiu ou não e as apresenta
+if frutas_iguais != set():
+    print(f'\nO conjunto de frutas em ambas as listas é: {frutas_iguais}')
+else:
+    print("\nNenhuma fruta coincide em ambas as listas")
